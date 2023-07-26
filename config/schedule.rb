d1c5a1304a5f6e33a81052908bfd4c0d2c641b5f
konchanxxx <rexit.org@gmail.com>
2023-07-26 18:17:56 +0900

    fix shell

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,8 +7,15 @@ set :environment, ENV['RAILS_ENV'] || :development
 # cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
 
+# zshが設定されている場合はSHELLをzshにする
+if ENV['SHELL'].include?('zsh')
+  env :SHELL, '/bin/zsh'
+elsif
+  env :SHELL, '/bin/bash'
+end
+
 # ジョブを実行するシェルをzshにする
-set :job_template, "source ~/.zshrc; $(which zsh) -l -c ':job'"
+set :job_template, "$SHELL -l -c ':job'"
 
 # 定期実行したい処理を記載
 every :minute do
